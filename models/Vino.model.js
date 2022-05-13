bbc480ae898e4410f55d779c26815c06b32ecb44
MariaDiezEsteve <diezesteve@outlook.es>
2022-05-13 13:40:19 +0200

    vo_vinos.json

--- models/Vino.model.js
+++ models/Vino.model.js
@@ -1,38 +1,44 @@
-const { Schema, model } = require("mongoose");
+const {
+    Schema,
+    model
+} = require("mongoose");
 
 const vinoSchema = new Schema({
     tipoVino: {
-      type: [Tinto, Blanco, Rosado],
-       required: true 
+        type: [Tinto, Blanco, Rosado],
+        required: true
     },
-  añada: {
-type: [Jóvenes, Crianza, Reserva, Gran-Reserva],
-          required: true 
+    añada: {
+        type: [Jóvenes, Crianza, Reserva, Gran - Reserva],
+        required: true
     },
- año:{
-      type: Number,
-      required: true 
+    año: {
+        type: Number,
+        required: true
     },
 
     denOrigen: {
-      type:String,
-enum : [ Calatayud, Cava, Machuela, Monterrei, Navarra, Rioja, Rueda, Terra-Alta, Uclés, Utiel, Valdeorras],
-required: true 
-    },   
-  
-puntuación:{
-      type: Number,
-enum : [0, 1, 2, 3, 4, 5]
+        type: String,
+        enum: [Calatayud, Cava, Machuela, Monterrei, Navarra, Rioja, Rueda, Terra - Alta, Uclés, Utiel, Valdeorras],
+        required: true
     },
-maridaje: {
-   type: String,
-enum:  [Ensaladas, Fiambres-y-Patés, Carnes, Pescados-y-Mariscos, Pasta-y-Arroz, Postres, Quesos, Chocolate],
-      required: true 
-},
-adminVinos: {
-type: Schema.Types.ObjectId,
-ref: "User"
-}
+
+    puntuación: {
+        type: Number,
+        enum: [0, 1, 2, 3, 4, 5]
+    },
+    maridaje: {
+        type: String,
+        enum: [Ensaladas, Fiambres - y - Patés, Carnes, Pescados - y - Mariscos, Pasta - y - Arroz, Postres, Quesos, Chocolate],
+        required: true
+    },
+    adminVinos: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    vinoPicture: {
+        type: String
+    }
 });
 
 const VinoModel = model("Vino", vinoSchema);
