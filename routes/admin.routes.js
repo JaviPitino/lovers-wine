495f7e658b7989e5049f27dee5dba7f3d80d6b76
Javier López <javierpitino@gmail.com>
2022-05-13 15:53:03 +0200

    V0 cloudinary

---
+++ routes/admin.routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+const UserModel = require("../models/User.model.js");
+const VinoModel = require("../models/Vino.model.js");
+
+// GET ("/admin") -> Mostrar vista del perfil de admin con la lista de vinos
+
+// POST ("/admin") -> 
+
+
+module.exports = router;
