51be79a4c3ce17e73bf53680843409bc4bd7d20d
MariaDiezEsteve <diezesteve@outlook.es>
2022-05-16 17:37:25 +0200

    V1

--- routes/wines.routes.js
+++ routes/wines.routes.js
@@ -18,52 +18,37 @@ router.get("/", (req, res, next) => {
 })
 
 // GET ("/wines/tinto") -> Mostrar vista con los vinos tintos
-router.get("/tinto", (req, res, next) => {
+router.get("/tinto", async (req, res, next) => {
 
-  let newArr = []
-  vinosCreados.forEach((eachWine) => {
-    if (eachWine.tipoVino === "Tinto") {
-      newArr.push(eachWine)
-    }
-  })
+  try {
+    const vinoTinto = await VinoModel.find({tipoVino: "Tinto"})
 
-  res.render("wines/tinto.hbs", {
-    vinoTinto: newArr
-  })
-
+    res.render("wines/tinto.hbs", {
+      vinoTinto
+    })
+  }catch(err){next(err)}
 })
 
+
+
 // GET ("/wines/blanco") -> Mostrar vista con los vinos blancos
-router.get("/blanco", (req, res, next) => {
-
-  let newArr = []
-  vinosCreados.forEach((eachWine) => {
-    if (eachWine.tipoVino === "Blanco") {
-      newArr.push(eachWine)
-    }
-  })
-
-  res.render("wines/blanco.hbs", {
-    vinoBlanco: newArr
-  })
-
-
+router.get("/blanco", async (req, res, next) => {
+  try {
+    const vinoBlanco = await VinoModel.find({tipoVino: "Blanco"})
+    res.render("wines/blanco.hbs", {
+      vinoBlanco
+    })
+  }catch(err){next(err)}
 })
 
 // GET ("/wines/rosado") -> Mostrar vista con los vinos rosados
 router.get("/rosado", async (req, res, next) => {
-
-    let newArr = []
-    vinosCreados.forEach((eachWine) => {
-      if (eachWine.tipoVino === "Rosado") {
-        newArr.push(eachWine)
-      }
-    })
-  
+  try {
+    const vinoRosado = await VinoModel.find({tipoVino: "Rosado"})
     res.render("wines/rosado.hbs", {
-      vinoRosado: newArr
+      vinoRosado
     })
-
+  }catch(err){next(err)}
 })
 
 // GET ("/wines/:id/details") -> Mostrar vista con los vinos rosados
@@ -78,7 +63,7 @@ router.get("/:id/details", async (req, res, next) => {
 })
 
 
-// POST ("/admin") -> 
+
 
 
 
